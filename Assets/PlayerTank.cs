ca0edf67f3b1a894e4feee3c740ab61a91405fe9
tmoritaa <tatsuya.ahad@gmail.com>
2018-01-09 22:19:21 -0500

    Modified movement to utilize Unity physics instead of all manual

--- Assets/PlayerTank.cs
+++ Assets/PlayerTank.cs
@@ -9,20 +9,20 @@ public class PlayerTank : MonoBehaviour
 {
     private Rigidbody2D body;
 
-    private float leftVel = 0f;
-    private float rightVel = 0f;
+    private float leftWheelPower = 0f;
+    private float rightWheelPower = 0f;
 
     [SerializeField]
-    private float accel = 0.1f;
+    private float powerIncPerTS = 0.1f;
 
     [SerializeField]
-    private float maxSpeed = 500f;
-
-    [SerializeField]
-    private float frictionDeterioration = 0.01f;
+    private float powerDeterPerTS = 0.01f;
 
     [SerializeField]
     private float width = 50f;
+
+    [SerializeField]
+    private float maxForcePerTS = 10000f;
 
     void Awake() {
         body = this.GetComponent<Rigidbody2D>();
@@ -37,56 +37,48 @@ public class PlayerTank : MonoBehaviour
     }
 
     private void handleInput() {
-        // Apply slow down due to friction.
-        if (Mathf.Abs(leftVel) > 0) {
-            leftVel = Mathf.Sign(leftVel) * (Mathf.Abs(leftVel) - frictionDeterioration);
-        }
-        if (Mathf.Abs(rightVel) > 0) {
-            rightVel = Mathf.Sign(rightVel) * (Mathf.Abs(rightVel) - frictionDeterioration);
-        }
-
-        // Force 0 if below sigma.
-        if (Mathf.Abs(leftVel) < 0.001f) {
-            leftVel = 0;
-        }
-        if (Mathf.Abs(rightVel) < 0.001f) {
-            rightVel = 0;
-        }
-
-        // Add acceleration and clamp based on key input.
+        // Add power increase and clamp based on key input.
         if (Input.GetKey(KeyCode.W)) {
-            leftVel += accel;
+            leftWheelPower += powerIncPerTS;
+        } else if (Input.GetKey(KeyCode.S)) {
+            leftWheelPower -= powerIncPerTS;
+        } else if (Mathf.Abs(leftWheelPower) > 0) {
+            leftWheelPower = Mathf.Sign(leftWheelPower) * (Mathf.Abs(leftWheelPower) - powerDeterPerTS);
         }
-        if (Input.GetKey(KeyCode.S)) {
-            leftVel -= accel;
-        }
-        leftVel = Mathf.Clamp(leftVel, -1.0f, 1.0f);
+        leftWheelPower = Mathf.Clamp(leftWheelPower, -1.0f, 1.0f);
 
         if (Input.GetKey(KeyCode.I)) {
-            rightVel += accel;
+            rightWheelPower += powerIncPerTS;
+        } else if (Input.GetKey(KeyCode.K)) {
+            rightWheelPower -= powerIncPerTS;
+        } else if (Mathf.Abs(rightWheelPower) > 0) {
+            rightWheelPower = Mathf.Sign(rightWheelPower) * (Mathf.Abs(rightWheelPower) - powerDeterPerTS);
         }
-        if (Input.GetKey(KeyCode.K)) {
-            rightVel -= accel;
+        rightWheelPower = Mathf.Clamp(rightWheelPower, -1.0f, 1.0f);
+
+        // Force 0 if below sigma.
+        if (Mathf.Abs(leftWheelPower) < 0.001f) {
+            leftWheelPower = 0;
         }
-        rightVel = Mathf.Clamp(rightVel, -1.0f, 1.0f);
+        if (Mathf.Abs(rightWheelPower) < 0.001f) {
+            rightWheelPower = 0;
+        }
     }
 
     private void handleMovement() {
         // Calculate rotation
-        Vector2 leftVec = new Vector2(-width / 2, leftVel);
-        Vector2 rightVec = new Vector2(width / 2, rightVel);
+        Vector2 leftVec = new Vector2(-width / 2, leftWheelPower);
+        Vector2 rightVec = new Vector2(width / 2, rightWheelPower);
 
-        Vector2 diffVec = new Vector2(rightVec.x - leftVec.x, rightVec.y - leftVec.y);
+        Vector2 diffVec = rightVec - leftVec;
         Vector2 perpUnitVec = new Vector2(-diffVec.y, diffVec.x).normalized;
 
         float finalAngle = Vector2.SignedAngle(new Vector2(0, 1.0f), perpUnitVec);
         this.body.rotation += finalAngle;
 
         // Calculate forward velocity
-        float finalVel = (leftVel + rightVel) / 2f * maxSpeed;
-        float radAngle = this.body.rotation * Mathf.PI / 180f;
-        Vector2 forwardVec = new Vector2(-Mathf.Sin(radAngle), Mathf.Cos(radAngle));
-
-        this.body.velocity = forwardVec.normalized * finalVel;
+        Vector2 forwardVec = new Vector2(0, 1).Rotate(this.body.rotation);
+        float finalVel = (leftWheelPower + rightWheelPower) / 2f * maxForcePerTS;
+        this.body.AddForce(forwardVec.normalized * finalVel, ForceMode2D.Force);
     }
 }
