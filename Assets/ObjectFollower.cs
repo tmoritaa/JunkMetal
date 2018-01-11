8e97b81327c630fadad78bf463099e9b540c5f24
tmoritaa <tatsuya.ahad@gmail.com>
2018-01-11 17:40:05 -0500

    Adjusted camera and added script to make camera follow player tank GO

---
+++ Assets/ObjectFollower.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+public class ObjectFollower : MonoBehaviour 
+{
+    [SerializeField]
+    GameObject objToFollow;
+
+    void Update() {
+        if (objToFollow != null) {
+            Vector3 newPos = objToFollow.transform.position;
+            newPos.z = this.transform.position.z;
+            this.transform.position = newPos;
+        }    
+    }
+}
