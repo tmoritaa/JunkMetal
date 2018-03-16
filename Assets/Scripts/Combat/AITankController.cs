2618e96870b51096ef754f50a360ffa8f3c2118f
tmoritaa <tatsuya.ahad@gmail.com>
2018-03-16 17:49:21 -0400

    Implemented new Attack goal

--- Assets/Scripts/Combat/AITankController.cs
+++ Assets/Scripts/Combat/AITankController.cs
@@ -73,7 +73,7 @@ public class AITankController : TankController
 
         // TODO: for now, just manually fill up goals list.
         //goals.Add(new SearchGoal(this));
-        //goals.Add(new AttackGoal(this));
+        goals.Add(new AttackGoal(this));
         goals.Add(new ManeuverGoal(this));
         curGoal = null;
     }
