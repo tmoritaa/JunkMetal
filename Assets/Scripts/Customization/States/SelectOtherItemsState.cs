360b43cd4b780f704446f350eba61fbe4ed75b1c
tmoritaa <tatsuya.ahad@gmail.com>
2018-03-21 14:01:53 -0400

    Added escape key for cancelling out of Customization screen

--- Assets/Scripts/Customization/States/SelectOtherItemsState.cs
+++ Assets/Scripts/Customization/States/SelectOtherItemsState.cs
@@ -29,7 +29,7 @@ public class SelectOtherItemsState : CustomizationState
     }
 
     public override void PerformUpdate() {
-        if (Input.GetKeyUp(KeyCode.Joystick1Button1) || Input.GetKeyUp(KeyCode.Mouse1)) {
+        if (Input.GetKeyUp(KeyCode.Joystick1Button1) || Input.GetKeyUp(KeyCode.Mouse1) || Input.GetKeyUp(KeyCode.Escape)) {
             handler.GotoState(CustomizationHandler.StateType.EquippedItemSelect);
         }
     }
