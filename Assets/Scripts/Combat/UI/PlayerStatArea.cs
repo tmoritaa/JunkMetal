a4c42cdc8c5adfd3fb1c38c4ebd38c5480055e54
tmoritaa <tatsuya.ahad@gmail.com>
2018-04-09 15:27:38 -0400

    Implemented Bar UI for energy, and mucked about with prefab

---
+++ Assets/Scripts/Combat/UI/PlayerStatArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+public class PlayerStatArea : MonoBehaviour 
+{
+    [SerializeField]
+    private bool displayEnergy;
+
+    [SerializeField]
+    private GameObject energyDisplay;
+
+	void Start()
+	{
+        energyDisplay.SetActive(displayEnergy);
+	}
+}
