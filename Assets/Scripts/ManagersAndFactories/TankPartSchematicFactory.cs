1e02d93bccc24295b58f2f46038a5b1aac8a687b
tmoritaa <tatsuya.ahad@gmail.com>
2018-04-09 12:20:57 -0400

    Added parameters for jets and energy, and loading from JSON for those parameters

--- Assets/Scripts/ManagersAndFactories/TankPartSchematicFactory.cs
+++ Assets/Scripts/ManagersAndFactories/TankPartSchematicFactory.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 
 public class TankParSchematictFactory
 {
-    public static HullPartSchematic CreateHullPartSchematic(string name, int armour, int energyPower, Vector2 size, int weight, float angularDrag, Vector2[] weaponDirs, Vector2[] weaponPos, PartSchematic.WeaponTier[] tierRestrictions) {
-        return new HullPartSchematic(name, armour, energyPower, size, weight, angularDrag, weaponDirs, weaponPos, tierRestrictions);
+    public static HullPartSchematic CreateHullPartSchematic(string name, int armour, int enginePower, Vector2 size, int weight, float angularDrag, int energy, int energyRefresh, float jetImpulse, int jetEnergyUsage, Vector2[] weaponDirs, Vector2[] weaponPos, PartSchematic.WeaponTier[] tierRestrictions) {
+        return new HullPartSchematic(name, armour, enginePower, size, weight, angularDrag, energy, energyRefresh, jetImpulse, jetEnergyUsage, weaponDirs, weaponPos, tierRestrictions);
     }
 
-    public static WeaponPartSchematic CreateWeaponPartSchematic(string name, float reloadTime, PartSchematic.WeaponTier weaponTier, Bullet.BulletTypes bulletType, Dictionary<string, object> bulletInfos) {
-        return new WeaponPartSchematic(name, reloadTime, weaponTier, bulletType, bulletInfos);
+    public static WeaponPartSchematic CreateWeaponPartSchematic(string name, float reloadTime, int energyUsage, PartSchematic.WeaponTier weaponTier, Bullet.BulletTypes bulletType, Dictionary<string, object> bulletInfos) {
+        return new WeaponPartSchematic(name, reloadTime, energyUsage, weaponTier, bulletType, bulletInfos);
     }
 }
